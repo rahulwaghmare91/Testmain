644e94c5c6ad465e0c0065fa87030f0329e4d92c
rahul <wagr372@gmail.com>
2022-08-06 09:06:51 -0700

    adding hi msg

--- src/main/java/practiceproject/test.java
+++ src/main/java/practiceproject/test.java
@@ -6,7 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class test {
 public static void main (String[] args) {
 	 System.setProperty("webdriver.chrome.driver","E:\\cucumber practice project\\Cucumber-for-paractice\\Drivers\\chromedriver.exe");
-   WebDriver   driver=new ChromeDriver();	
+   WebDriver   driver=new ChromeDriver();
+   System.out.println("hi");
 
 }
 }
